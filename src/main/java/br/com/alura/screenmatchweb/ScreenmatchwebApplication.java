064658082c901584da5d44472b056fab864dd7d5
danielmjob <danielmarquesjob@gmail.com>
2023-06-12 16:32:25 -0300

    Iniciando estrutura do projeto

---
+++ src/main/java/br/com/alura/screenmatchweb/ScreenmatchwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatchweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchwebApplication.class, args);
+	}
+
+}

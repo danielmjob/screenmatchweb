eac67a50342bd00e1ab70f9ae26296013a132ebd
danielmjob <danielmarquesjob@gmail.com>
2023-06-15 16:48:26 -0300

    implementado o Controlador

---
+++ src/main/java/br/com/alura/screenmatchweb/controller/FilmeController.java
@@ -0,0 +1,15 @@
+package br.com.alura.screenmatchweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/filmes") /* mapeando a requisição */
+public class FilmeController {
+    @GetMapping /* quando (acessar o endereço /filmes) requisição do tipo get vai acionar esse metodo*/
+    public String carregaPaginaFormulario(){
+        return "filmes/formulario"; /* nome da pagina HTML que será acessada*/
+    }
+
+}

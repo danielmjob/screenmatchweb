2d7241a7fa0a2d5a6ca115428eb42caae35e0763
danielmjob <danielmarquesjob@gmail.com>
2023-06-15 17:38:11 -0300

    Cadastrando Filmes

--- src/main/java/br/com/alura/screenmatchweb/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatchweb/controller/FilmeController.java
@@ -1,21 +1,31 @@
 package br.com.alura.screenmatchweb.controller;
 
 import br.com.alura.screenmatchweb.model.filme.DadosCadastroFilme;
+import br.com.alura.screenmatchweb.model.filme.Filme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/filmes") /* mapeando a requisição */
 public class FilmeController {
+
+    private List<Filme> filmes = new ArrayList<>();
+
     @GetMapping /* quando (acessar o endereço /filmes) requisição do tipo get vai acionar esse metodo*/
     public String carregaPaginaFormulario(){
         return "filmes/formulario"; /* nome da pagina HTML que será acessada*/
     }
     @PostMapping /*envia dados do formulário*/
     public String cadastraFilme(DadosCadastroFilme dados){ /*Classe que recebe os dados do formulario, poderia tbm colocar eles um por um avulso mas geraria codigo dififil de manutenção*/
-        System.out.println(dados);
+        var filme = new Filme(dados);
+        filmes.add(filme);
+
+        System.out.println(filmes); /*verifica se foi salvo na lista*/
         return "filmes/formulario";
     }
 

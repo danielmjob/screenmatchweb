99d2257da1ffc4e403dc0cc03e8e2c2f46a7e294
danielmjob <danielmarquesjob@gmail.com>
2023-06-15 17:24:24 -0300

    Recebendo dados no Controller

--- src/main/java/br/com/alura/screenmatchweb/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatchweb/controller/FilmeController.java
@@ -1,15 +1,22 @@
 package br.com.alura.screenmatchweb.controller;
 
+import br.com.alura.screenmatchweb.model.filme.DadosCadastroFilme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/filmes/") /* mapeando a requisição */
+@RequestMapping("/filmes") /* mapeando a requisição */
 public class FilmeController {
     @GetMapping /* quando (acessar o endereço /filmes) requisição do tipo get vai acionar esse metodo*/
     public String carregaPaginaFormulario(){
         return "filmes/formulario"; /* nome da pagina HTML que será acessada*/
+    }
+    @PostMapping /*envia dados do formulário*/
+    public String cadastraFilme(DadosCadastroFilme dados){ /*Classe que recebe os dados do formulario, poderia tbm colocar eles um por um avulso mas geraria codigo dififil de manutenção*/
+        System.out.println(dados);
+        return "filmes/formulario";
     }
 
 }
---
+++ src/main/java/br/com/alura/screenmatchweb/model/filme/DadosCadastroFilme.java
@@ -0,0 +1,5 @@
+package br.com.alura.screenmatchweb.model.filme;
+
+/*usado tipo record pois não havera alterações diretas e nesse tipo ja tem get e set auto implementado*/
+public record DadosCadastroFilme(String nome, Integer duracao, Integer ano, String genero) {
+}
